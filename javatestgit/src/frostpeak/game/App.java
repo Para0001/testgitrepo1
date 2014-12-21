9a1b48b79926c74b36c83e6c3b28965b6dc80b6a
pilapodapostache <pilapodapostache@asdf>
2014-12-21 11:11:35 -0600

    Cleaned up the excess println's and replaced with one.

--- javatestgit/src/frostpeak/game/App.java
+++ javatestgit/src/frostpeak/game/App.java
@@ -3,19 +3,7 @@ package frostpeak.game;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Git Test");
-		System.out.println("New Thing!");
-		System.out.println("You got Git Pull working");
-		System.out.println("Something");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
-		System.out.println("You got Git Pull working");
+		System.out.println("Nice and clean. :)");
 	}
 
 }
