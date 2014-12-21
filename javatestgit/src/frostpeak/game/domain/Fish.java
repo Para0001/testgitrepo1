35ee62c4ddaaebab00ec3be223b8a14e879a962f
Para0001 <none>
2014-12-21 17:58:38 -0500

    Added stuff.

--- javatestgit/src/frostpeak/game/domain/Fish.java
+++ javatestgit/src/frostpeak/game/domain/Fish.java
@@ -1,5 +1,11 @@
 package frostpeak.game.domain;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Fish {
 
@@ -7,14 +13,67 @@ public class Fish {
 	private int age;
 	private FishType type;
 	
+	private Random rand = new Random();
+	
 	public Fish(){
-		name = "Frank";
-		age = 10;
-		type = FishType.STUPID;
+		name = getRandomName();
+		age = rand.nextInt(10);
+		type = getRandomFishType();
 	}
 	
+	public Fish(String name, int age, FishType type){
+		this.name = name;
+		this.age = age;
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public FishType getType() {
+		return type;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setType(FishType type) {
+		this.type = type;
+	}
+	
+	private String getRandomName() {
+		try {
+			Scanner scan = new Scanner(new File("Names.txt"));
+			ArrayList<String> names = new ArrayList<String>();
+			
+			while(scan.hasNext()){
+				names.add(scan.next());
+			}
+			scan.close();
+			
+			return names.get(rand.nextInt(names.size()));
+		} catch (FileNotFoundException e) {
+			return "Bob";
+		}
+	}
+	
+	public FishType getRandomFishType() {
+		ArrayList<FishType> fishtypes = FishType.getFishTypes();
+		return fishtypes.get(rand.nextInt(fishtypes.size()));
+	}
+
 	public String toString(){
-		return "This fish is: " + name +
-				". He is " + age + " years old. He is: " + type;
+		return "This fish's name is " + name +
+				". He is " + age + " years old. He is of type " + type + ".";
 	}
 }
--- javatestgit/src/frostpeak/game/domain/FishType.java
+++ javatestgit/src/frostpeak/game/domain/FishType.java
@@ -1,9 +1,20 @@
 package frostpeak.game.domain;
 
+import java.util.ArrayList;
+
 public enum FishType {
 	GOLDFISH,
 	BASS,
 	TROUT,
 	RAINBOW,
-	STUPID
-};
+	STUPID;
+	
+	public static ArrayList<FishType> getFishTypes(){
+		ArrayList<FishType> fishtypes = new ArrayList<FishType>();
+		for(FishType fishtype : FishType.values()){
+			fishtypes.add(fishtype);
+		}
+		return fishtypes;
+	}
+}
+
