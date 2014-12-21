dd6336319992c8b0137d9f0959ad282e7c4296ab
Para0001 <none>
2014-12-20 20:18:50 -0500

    something

--- javatestgit/src/frostpeak/game/App.java
+++ javatestgit/src/frostpeak/game/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
 		System.out.println("New Thing!");
+		System.out.println("You got Git Pull working");
 	}
 
 }
