e89f0c5e6862a6837d07971a148d16439c5af38c
pilapodapostache <pilapodapostache@Scott-Laptop.gateway.mts.net>
2014-12-20 19:26:47 -0600

    test commit

--- javatestgit/src/frostpeak/game/App.java
+++ javatestgit/src/frostpeak/game/App.java
@@ -6,6 +6,15 @@ public class App {
 		System.out.println("Git Test");
 		System.out.println("New Thing!");
 		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
+		System.out.println("You got Git Pull working");
 	}
 
 }
